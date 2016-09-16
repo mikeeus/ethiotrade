136d6e547d047705c877d720c9fc6373839292bc
mikias <itsmikias@gmail.com>
2016-09-16 16:01:44 +0300

    Add empty search component

---
+++ ang-client/src/app/search/search.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search',
+  templateUrl: 'search.html',
+  styleUrls: ['search.scss']
+})
+export class Search implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
