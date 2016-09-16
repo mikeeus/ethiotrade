6e75da443146cb09589f4649d362f9504f7d20dd
mikias <itsmikias@gmail.com>
2016-09-16 18:50:40 +0300

    Generate hscodes scaffold

--- db/structure.sql
+++ db/structure.sql
@@ -46,12 +46,60 @@ CREATE TABLE ar_internal_metadata (
 
 
 --
+-- Name: hscodes; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE hscodes (
+    id integer NOT NULL,
+    code integer NOT NULL,
+    description character varying NOT NULL,
+    unit character varying DEFAULT 'UN'::character varying NOT NULL,
+    special_permission character varying DEFAULT 'None'::character varying NOT NULL,
+    duty integer DEFAULT 0 NOT NULL,
+    excise integer DEFAULT 0 NOT NULL,
+    vat integer DEFAULT 0 NOT NULL,
+    sur integer DEFAULT 0 NOT NULL,
+    withhold integer DEFAULT 0 NOT NULL,
+    ss1 integer DEFAULT 0 NOT NULL,
+    ss2 integer DEFAULT 0 NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: hscodes_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE hscodes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: hscodes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE hscodes_id_seq OWNED BY hscodes.id;
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE schema_migrations (
     version character varying NOT NULL
 );
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY hscodes ALTER COLUMN id SET DEFAULT nextval('hscodes_id_seq'::regclass);
 
 
 --
@@ -63,6 +111,14 @@ ALTER TABLE ONLY ar_internal_metadata
 
 
 --
+-- Name: hscodes_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY hscodes
+    ADD CONSTRAINT hscodes_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -71,11 +127,25 @@ ALTER TABLE ONLY schema_migrations
 
 
 --
+-- Name: index_hscodes_on_code; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_hscodes_on_code ON hscodes USING btree (code);
+
+
+--
+-- Name: index_hscodes_on_description; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_hscodes_on_description ON hscodes USING btree (description);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
 SET search_path TO "$user", public;
 
-INSERT INTO schema_migrations (version) VALUES ;
+INSERT INTO schema_migrations (version) VALUES ('20160916154634');
 
 
