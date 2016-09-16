98fb43e865a2559b9279463257dbe3f5286e2634
mikias <itsmikias@gmail.com>
2016-09-16 10:10:00 +0300

    Add comments, add empty style sheet

--- ang-client/src/app/layout/header.ts
+++ ang-client/src/app/layout/header.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {DropdownDirective, CollapseDirective} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'Header',
-  templateUrl: 'header.html'
+  templateUrl: 'header.html',
+  styleUrls: ['header.scss']
 })
 export class Header implements OnInit {
   constructor() { }
 
+  public isCollapsed: boolean;
+
   ngOnInit() { }
+
 }
