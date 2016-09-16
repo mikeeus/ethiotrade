243acbaf17bdcd58195067b2b2c54740af4a79d8
mikias <itsmikias@gmail.com>
2016-09-16 12:31:32 +0300

    Change links to routerLinks

--- ang-client/src/app/layout/header/header.ts
+++ ang-client/src/app/layout/header/header.ts
@@ -7,6 +7,13 @@ import {DropdownDirective, CollapseDirective} from 'ng2-bootstrap/ng2-bootstrap'
   styleUrls: ['header.scss']
 })
 export class Header implements OnInit {
+  countries: string[] = [
+    'Canada',
+    'Ethiopia'
+  ];
+  years: string[] =  ['1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
+  showYearsDropdown: boolean = false;
+
   constructor() { }
 
   public isCollapsed: boolean;
