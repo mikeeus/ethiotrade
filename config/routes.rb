47b40cf69dfb9db6615b9b6aa208dc3ef4d15dc1
mikias <itsmikias@gmail.com>
2016-09-16 19:06:01 +0300

    Add search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :hscodes
+  scope 'api' do
+    get '/hscodes/search/:search_terms' to: 'hscodes#search' 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
