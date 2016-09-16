4203a483a24f0f73e93b8684ff69a1c198579fcb
mikias <itsmikias@gmail.com>
2016-09-16 10:00:59 +0300

    Create header

---
+++ ang-client/src/app/layout/header.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Header',
+  templateUrl: 'header.html'
+})
+export class Header implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
