d2a92294a84fd018a92b997462c37cdd72b4f0c3
mikias <itsmikias@gmail.com>
2016-09-16 17:19:55 +0300

    Add rough search component

--- ang-client/src/app/search/search.ts
+++ ang-client/src/app/search/search.ts
@@ -1,12 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'search',
-  templateUrl: 'search.html',
+  // templateUrl: 'search.html',
+  template: `
+    <form [formGroup]="searchForm">
+      <input
+        type="text"
+        placeholder="Search for GitHub repositories..."
+        formControlName="search">
+    </form>
+  `,
   styleUrls: ['search.scss']
 })
 export class Search implements OnInit {
-  constructor() { }
+  private searchForm: FormGroup;
+  private search: FormControl = new FormControl();
 
-  ngOnInit() { }
+  @Output() private onSearch: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(fb: FormBuilder) {
+    this.searchForm = fb.group({ search: this.search });
+   }
+
+  ngOnInit() {
+    this.searchForm.valueChanges
+        .map(value => value.search)
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .subscribe((terms: string) => this.onSearch.emit(terms))
+   }
 }
