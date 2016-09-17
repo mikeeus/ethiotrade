a73c9d1890622e6f56b8cc22c8f6b4449d6fa24c
mikias <itsmikias@gmail.com>
2016-09-17 21:39:41 +0300

    Add error handling

--- ang-client/src/app/charts/chart.service.ts
+++ ang-client/src/app/charts/chart.service.ts
@@ -2,27 +2,26 @@ import { Injectable } from '@angular/core';
 import { Http, Response, ResponseOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { HscodeDetail } from '../models';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ServiceHelpers } from '../helpers';
 
 @Injectable()
 export class ChartService {
   hscodeUrl:string = '/api/charts/hscodes/'
   
   constructor(
-    private http: Http
+    private http: Http,
+    private sH: ServiceHelpers
   ) { }
 
   getHscode(code: number): Observable<HscodeDetail> {
     return this.http.get(`${this.hscodeUrl}/${code}`)
-        // check for errors
-        // .map()
-        // catch errors
-        .map(res => this.getJson(res))
-        // getJSon
+        .map(this.sH.getJson)
+        .catch(this.sH.handleError)
   }
 
-  private getJson(response) {
-    return response.json();
-  }
+
 
   getCountry() {
 
