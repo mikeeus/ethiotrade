64c5f50540044d816b1374167d141abb740dc0a8
mikias <itsmikias@gmail.com>
2016-09-17 10:47:58 +0300

    Remove unused methods

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,42 +1,5 @@
 class ExportsController < ApplicationController
-  before_action :set_export, only: [:show, :update, :destroy]
-
-  # GET /exports
-  def index
-    @exports = Export.all
-
-    render json: @exports
-  end
-
-  # GET /exports/1
-  def show
-    render json: @export
-  end
-
-  # POST /exports
-  def create
-    @export = Export.new(export_params)
-
-    if @export.save
-      render json: @export, status: :created, location: @export
-    else
-      render json: @export.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /exports/1
-  def update
-    if @export.update(export_params)
-      render json: @export
-    else
-      render json: @export.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /exports/1
-  def destroy
-    @export.destroy
-  end
+  # before_action :set_export, only: [:show, :update, :destroy]
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,42 +1,5 @@
 class ImportsController < ApplicationController
-  before_action :set_import, only: [:show, :update, :destroy]
-
-  # GET /imports
-  def index
-    @imports = Import.all
-
-    render json: @imports
-  end
-
-  # GET /imports/1
-  def show
-    render json: @import
-  end
-
-  # POST /imports
-  def create
-    @import = Import.new(import_params)
-
-    if @import.save
-      render json: @import, status: :created, location: @import
-    else
-      render json: @import.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /imports/1
-  def update
-    if @import.update(import_params)
-      render json: @import
-    else
-      render json: @import.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /imports/1
-  def destroy
-    @import.destroy
-  end
+  # before_action :set_import, only: [:show, :update, :destroy]
 
   private
     # Use callbacks to share common setup or constraints between actions.
