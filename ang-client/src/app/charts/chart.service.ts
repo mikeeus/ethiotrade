91ab9243a82147ab5ba6017cefad3fb163fdd55e
mikias <itsmikias@gmail.com>
2016-09-17 21:43:10 +0300

    Add country get method

--- ang-client/src/app/charts/chart.service.ts
+++ ang-client/src/app/charts/chart.service.ts
@@ -8,28 +8,36 @@ import { ServiceHelpers } from '../helpers';
 
 @Injectable()
 export class ChartService {
-  hscodeUrl:string = '/api/charts/hscodes/'
+  homepageUrl: string = 'api/charts/homepage';
+  hscodesUrl: string = '/api/charts/hscodes/';
+  countriesUrl: string = '/api/charts/countries/';
   
   constructor(
     private http: Http,
     private sH: ServiceHelpers
   ) { }
 
+  getHomepage(): Observable<any> {
+    return this.http.get(this.homepageUrl)
+      .map(this.sH.getJson)
+      .catch(this.sH.handleError)      
+  }
+  
   getHscode(code: number): Observable<HscodeDetail> {
-    return this.http.get(`${this.hscodeUrl}/${code}`)
-        .map(this.sH.getJson)
-        .catch(this.sH.handleError)
+    return this.http.get(`${this.hscodesUrl}/${code}`)
+      .map(this.sH.getJson)
+      .catch(this.sH.handleError)
   }
 
-
-
-  getCountry() {
-
+  getCountry(country: string): Observable<CountryDetail> {
+    return this.http.get(`${this.countriesUrl}/${country}`)
+      .map(this.sH.getJson)
+      .catch(this.sH.handleError)
   }
 
   // Should get both chart and table data
-  getYear() {
+  // getYear() {
 
-  }
+  // }
 
 }
