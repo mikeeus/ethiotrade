1a83ff2a3b4eee29f80356819c3832dac5e16ef9
mikias <itsmikias@gmail.com>
2016-09-17 12:16:40 +0300

    Add test for tables method

--- test/controllers/countries_controller_test.rb
+++ test/controllers/countries_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CountriesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @import = imports(:one)
+  end
+  
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get tables" do
+    get country_tables_url(
+      @import.country_origin, 'Import', @import.year), as: :json
+    assert_response :success
+  end
 end
