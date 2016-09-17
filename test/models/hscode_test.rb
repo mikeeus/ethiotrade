c808e91661cc59ae2e530dcb0e1d516f481c75bc
mikias <itsmikias@gmail.com>
2016-09-17 14:11:22 +0300

    Add hscode search tests

--- test/models/hscode_test.rb
+++ test/models/hscode_test.rb
@@ -10,4 +10,23 @@ class HscodeTest < ActiveSupport::TestCase
   # end
 
   # Test related codes model
+  
+  def setup
+    @search_description = HscodeSearch.new('First')
+    @search_code = HscodeSearch.new('2000')
+    @hscode_first = hscodes(:one)
+    @hscode_second = hscodes(:two)
+  end
+
+  test "should search hscodes by name" do
+    response = @search_description.search
+    assert_equal response[0], @hscode_first
+    assert_not_equal response[0], @hscode_second
+  end
+
+  test "should search hscodes by code" do
+    response = @search_code.search
+    assert_not_equal response[0], @hscode_first
+    assert_equal response[0], @hscode_second
+  end
 end
