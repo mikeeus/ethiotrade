488475571984e3563c76829d15409d9657c3dd74
mikias <itsmikias@gmail.com>
2016-09-17 08:55:40 +0300

    Update methods

--- app/controllers/hscodes_controller.rb
+++ app/controllers/hscodes_controller.rb
@@ -1,5 +1,27 @@
 class HscodesController < ApplicationController
-  before_action :set_hscode, only: [:show, :update, :destroy]
+  before_action :set_hscode, only: [:show, :tables, :update, :destroy]
+
+  # GET /hscodes
+  def index
+    @hscodes = Hscode.all.to_a.slice(1..-1)
+
+    render json: @hscodes
+  end
+
+  # GET /hscodes/1
+  def show
+    render json: {
+      hscode: @hscode,
+      relatedCodes: @hscode.related_codes
+    }
+  end
+
+  def tables
+    render json: {
+      imports: @hscode.imports,
+      exports: @hscode.exports
+    }
+  end
 
   def search
     search_term = params[:search]
@@ -8,47 +30,10 @@ class HscodesController < ApplicationController
   end
 
 
-  # GET /hscodes
-  def index
-    @hscodes = Hscode.all
-
-    render json: @hscodes
-  end
-
-  # GET /hscodes/1
-  def show
-    render json: @hscode
-  end
-
-  # POST /hscodes
-  def create
-    @hscode = Hscode.new(hscode_params)
-
-    if @hscode.save
-      render json: @hscode, status: :created, location: @hscode
-    else
-      render json: @hscode.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /hscodes/1
-  def update
-    if @hscode.update(hscode_params)
-      render json: @hscode
-    else
-      render json: @hscode.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /hscodes/1
-  def destroy
-    @hscode.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_hscode
-      @hscode = Hscode.find(params[:id])
+      @hscode = Hscode.find_by(code: params[:code])
     end
 
     # Only allow a trusted parameter "white list" through.
