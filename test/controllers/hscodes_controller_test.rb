a7de67ece6fe79ac452e2b1de87cc023dce1bc25
mikias <itsmikias@gmail.com>
2016-09-17 09:10:45 +0300

    Configure tests

--- test/controllers/hscodes_controller_test.rb
+++ test/controllers/hscodes_controller_test.rb
@@ -10,29 +10,15 @@ class HscodesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create hscode" do
-    assert_difference('Hscode.count') do
-      post hscodes_url, params: { hscode: {  } }, as: :json
-    end
-
-    assert_response 201
-  end
-
   test "should show hscode" do
     get hscode_url(@hscode), as: :json
     assert_response :success
   end
 
-  test "should update hscode" do
-    patch hscode_url(@hscode), params: { hscode: {  } }, as: :json
-    assert_response 200
-  end
+  # search
+    
 
-  test "should destroy hscode" do
-    assert_difference('Hscode.count', -1) do
-      delete hscode_url(@hscode), as: :json
-    end
+  # tables
+    # renders imports and exports of hscode
 
-    assert_response 204
-  end
 end
--- test/models/hscode_test.rb
+++ test/models/hscode_test.rb
@@ -3,5 +3,11 @@ require 'test_helper'
 class HscodeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
+  # hscodes(:one)
+  # hscodes(:two)
+  # hscodes(:one_rel)
+  # hscodes(:one_not_rel)
   # end
+
+  # Test related codes model
 end
