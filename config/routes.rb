c56ee8c54582b55d576df86b464a4879a0558556
mikias <itsmikias@gmail.com>
2016-09-17 08:45:18 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
-  resources :exports
-  resources :imports
-  scope 'api' do
-    get '/hscodes/search/:search_term', to: 'hscodes#search' 
+  scope '/api' do
+    resources :exports, only: [:index, :show]
+    resources :imports, only: [:index, :show]
+    resources :hscodes, only: [:index]
   end
+ 
+  # Hscodes
+  get '/api/hscodes/:code' => 'hscodes#show', as: :hscode
+  get '/api/hscodes/search/:search' => 'hscodes#search', as: :hscodes_search
+
+  # Tables
+  get '/api/hscodes/:code/tables' => 'hscodes#tables', as: :hscode_tables
+
+  # Charts/ Matviews routes
+  get '/api/charts/homepage' => 'matviews#homepage', as: :homepage
+  get '/api/charts/country/:country' => 'matviews#country', as: :country_chart
+  get '/api/charts/year/:year' => 'matviews#year', as: :year
+  get '/api/charts/hscode/:code' => 'matviews#hscode', as: :hscode_matview
+  get '/api/refresh_matviews' => 'matviews#refresh_matviews', as: :refresh_matviews
+
+  get '/api/country/:country' => 'matviews#country', as: :country
+  get '/api/year/:year' => 'matviews#year'
+  get '/api/year/:year/summary' => 'matviews#year_summary'
+  get '/api/hscode/:code' => 'matviews#hscode'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
