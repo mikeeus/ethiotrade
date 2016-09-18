304d5195710600cda420c25faaaec43a428e8a50
mikias <itsmikias@gmail.com>
2016-09-18 18:56:47 +0300

    Add store and initialize

--- ang-client/src/app/hscode/hscode-detail/hscode-detail.ts
+++ ang-client/src/app/hscode/hscode-detail/hscode-detail.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { HscodeData } from '../../models';
+import { HscodeService } from '../hscode.service';
+import { GET_HSCODE } from '../../reducers/hscode-detail';
 
 @Component({
   selector: 'app-hscode',
@@ -10,13 +13,23 @@ import { HscodeData } from '../../models';
 })
 export class HscodeDetail implements OnInit {
   hscodeDetail: Observable<HscodeData>;
+  code: number;
 
   constructor(
-    private store: Store<any>
+    private store: Store<any>,
+    private hscodeService: HscodeService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.code = +params['code'];
+    })
     this.hscodeDetail = this.store.select('hscodeDetail');
+    this.hscodeService.getHscodeDetail(this.code)
+        .subscribe(res => {
+          this.store.dispatch({type: GET_HSCODE, payload: res})
+        });
   }
 
 }
