65beec52150993731580e41ab7abf9a6bf7dcac0
mikias <itsmikias@gmail.com>
2016-09-18 18:23:11 +0300

    Create hscode service

---
+++ ang-client/src/app/hscode/hscode.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HscodeService } from './hscode.service';
+
+describe('Service: Hscode', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [HscodeService]
+    });
+  });
+
+  it('should ...', inject([HscodeService], (service: HscodeService) => {
+    expect(service).toBeTruthy();
+  }));
+});
---
+++ ang-client/src/app/hscode/hscode.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { ServiceHelpers } from '../helpers';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class HscodeService {
+  hscodeDetailUrl: string = '/api/hscodes/'
+
+  constructor(
+    private store: Store<any>,
+    private http: Http,
+    private sH: ServiceHelpers
+  ) { }
+
+  getHscodeDetail(code: number) {
+    this.http.get(`${this.hscodeDetailUrl}/${code}`)
+        .map(this.sH.getJson)
+        .catch(this.sH.handleError)
+  }
+
+  // Get hscodeTable, takes params type, year, page, page_length 
+
+}
