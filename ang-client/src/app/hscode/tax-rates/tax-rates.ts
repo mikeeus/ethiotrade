826a125c646f71ad9efa9f116d171effde91ae66
mikias <itsmikias@gmail.com>
2016-09-18 21:29:58 +0300

    Add tax-rates component

--- ang-client/src/app/hscode/tax-rates/tax-rates.ts
+++ ang-client/src/app/hscode/tax-rates/tax-rates.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Hscode } from '../../models';
 
 @Component({
   selector: 'tax-rates',
@@ -6,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./tax-rates.scss']
 })
 export class TaxRates implements OnInit {
-  @Input() taxRates;
+  @Input() hscode: Hscode;
 
   constructor() { }
 
