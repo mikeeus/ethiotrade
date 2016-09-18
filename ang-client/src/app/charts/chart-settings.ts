47ebc3259abd7c4ea294f8c00d4c0abe068ba267
mikias <itsmikias@gmail.com>
2016-09-18 12:45:44 +0300

    Refactor chart settings

---
+++ ang-client/src/app/charts/chart-settings.ts
@@ -0,0 +1,44 @@
+// add formatting for labels similar to ticks
+
+export const chartOptions = {
+  scaleShowVerticalLines: false,
+  responsive: true,
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero:true,
+        userCallback: function (value){
+          if (value < 1){
+            return "";
+          } else if (value < 10000){
+            return "$" + value;
+          } else if (value < 1000000){
+            return "$" + (value/1000)+",000";
+          } else if (value < 1000000000){
+            return "$" + value/1000000 + " Million";
+          } else {
+            return "$" + value/1000000000 + " Billion";
+          }
+        }
+      }
+    }],
+    xAxes: [{
+      ticks: {
+        userCallback: function(value) {
+
+        if ( value.length > 15 ){
+          return value.slice(0,13) + "...";
+        }
+        return value;
+        }
+      }
+    }]
+  }
+}
+
+export const chartColors = [
+  // Imports
+  { backgroundColor: '#ff9800' },
+  // Exports
+  { backgroundColor: '#4caf50' }
+]
--- ang-client/src/app/charts/homepage/homepage.ts
+++ ang-client/src/app/charts/homepage/homepage.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { AnnualChart } from '../../models';
 import { YEARS } from '../../shared';
 import { Store } from '@ngrx/store';
+import { chartOptions, chartColors } from '../chart-settings';
 
 @Component({
   selector: 'homepage-chart',
@@ -9,6 +10,7 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./homepage.scss']
 })
 export class HomepageChart implements OnInit {
+  // Chart data is input from parent component asynchronously
   @Input() chartData;
 
   constructor() { }
@@ -16,65 +18,19 @@ export class HomepageChart implements OnInit {
   ngOnInit() {
   }
 
-
   // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = YEARS.reverse();
-  public lineChartOptions:any = {
-    animation: false,
-    responsive: true
-  };
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
+  public chartType:string = 'bar';
+  public chartLegend:boolean = true;
+  public chartOptions:any = chartOptions;
+  public chartColors:Array<any> = chartColors;
+  public chartLabels:Array<any> = YEARS.reverse();
 
   // events
   public chartClicked(e:any):void {
-    console.log(e);
+    // console.log(e);
   }
-
   public chartHovered(e:any):void {
-    console.log(e);
+    // console.log(e);
   }
 
 }
