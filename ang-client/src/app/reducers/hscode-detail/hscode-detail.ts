0ebe81a67afef21077c9329843e8ae73963277ae
mikias <itsmikias@gmail.com>
2016-09-19 10:34:01 +0300

    Correct typo

--- ang-client/src/app/reducers/hscode-detail/hscode-detail.ts
+++ ang-client/src/app/reducers/hscode-detail/hscode-detail.ts
@@ -1,10 +1,23 @@
 import { Action, ActionReducer } from '@ngrx/store';
 import { Hscode } from '../../models';
 
-export const GET_HSCODE = "SET_HSCODE";
+export const GET_HSCODE = "GET_HSCODE";
 export const REMOVE_HSCODE = "REMOVE_HSCODE";
 
-export const HscodeDetailReducer: ActionReducer<Hscode> = (state: Hscode, action: Action) => {
+const initialState = {
+  id: 0,
+  code: 0,
+  description: '',
+  unit: '',
+  duty: 0,
+  excise: 0,
+  special_permission: '',
+  sur: 0,
+  vat: 0,
+  withhold: 0,
+}
+
+export const HscodeDetailReducer: ActionReducer<Hscode> = (state: Hscode = initialState, action: Action) => {
   switch (action.type) {
     case GET_HSCODE:
       return action.payload;
