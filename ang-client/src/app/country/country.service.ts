8353fbdc0aa26f60d203de8a7b1a244684e62b7d
mikias <itsmikias@gmail.com>
2016-09-20 20:57:15 +0300

    Create country service

--- ang-client/src/app/country/country.service.ts
+++ ang-client/src/app/country/country.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { ServiceHelpers } from '../helpers';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CountryService {
+  countryUrl: string = '/api/countries/'
 
-  constructor() { }
+  constructor(
+    private http: Http,
+    private sH: ServiceHelpers
+  ) { }
+
+  getCountryDetail(country: string): Observable<any> {
+    return this.http.get(`${this.countryUrl}/${country}`)
+      .map(this.sH.getJson)
+      .catch(this.sH.handleError)
+  }
 
 }
