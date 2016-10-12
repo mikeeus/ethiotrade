85ed2aa11b23d278516c8e135c6550f60215d194
mikeeus <itsmikias@gmail.com>
2016-10-12 18:21:46 +0300

    Remove interface

--- ang-client/src/typings.d.ts
+++ ang-client/src/typings.d.ts
@@ -2,11 +2,5 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-interface NodeRequire {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-}
-
 declare var System: any;
-declare var require: NodeRequire;
+declare var require: any;
